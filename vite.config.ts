a9327e571f4d16bf0439350374c42a5dce1cc5cc
Meeken <meeken1998@gmail.com>
2024-08-30 11:33:39 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
@@ -11,5 +12,10 @@ export default defineConfig({
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
     include: ['**/test.{ts,tsx}']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
-})
+});
